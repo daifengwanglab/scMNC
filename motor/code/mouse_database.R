e74b2bb9fee12a9506ca5e0b4cca53df6a581daa
Jiawei98 <70461753+Jiawei98@users.noreply.github.com>
2021-01-17 10:14:24 -0600

    Delete mouse_database.R

--- motor/code/mouse_database.R
+++
@@ -1,8 +0,0 @@
-setwd(getwd())
-dbFiles <- c("https://resources.aertslab.org/cistarget/databases/mus_musculus/mm9/refseq_r45/mc9nr/gene_based/mm9-500bp-upstream-7species.mc9nr.feather",
-             "https://resources.aertslab.org/cistarget/databases/mus_musculus/mm9/refseq_r45/mc9nr/gene_based/mm9-tss-centered-10kb-7species.mc9nr.feather")
-dir.create("cisTarget_databases"); setwd("cisTarget_databases") # if needed
-for(featherURL in dbFiles)
-{
-  download.file(featherURL, destfile=basename(featherURL)) # saved in current dir
-}
